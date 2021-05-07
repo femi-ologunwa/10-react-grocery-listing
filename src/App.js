7fc927d1b3ef89d2db4729bb32943dadd9c1205e
Femi <ologunwafemi@gmail.com>
2021-05-07 19:09:07 +0100

    added local storage functionality to store list of items

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+   let list = localStorage.getItem('list');
+   if (list) {
+      return JSON.parse(localStorage.getItem('list'));
+   } else {
+      return [];
+   }
+};
+
 function App() {
    const [name, setName] = useState('');
-   const [list, setList] = useState([]);
+   const [list, setList] = useState(getLocalStorage());
    const [isEditing, setIsEditing] = useState(false);
    const [editID, setEditID] = useState('');
    const [alert, setAlert] = useState({
@@ -67,6 +76,11 @@ function App() {
       setEditID(id);
       setName(specificItem.title);
    };
+
+   //store in the local storage the latest changes to the list
+   useEffect(() => {
+      localStorage.setItem('list', JSON.stringify(list));
+   }, [list]);
 
    return (
       <section className='section-center'>
